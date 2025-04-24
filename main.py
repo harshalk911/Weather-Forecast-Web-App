e0c259ffa57f494461cc2ebdad045b7a31e3f195
Harshal Korade <korade54321@gmail.com>
2025-04-25 03:14:15 +0530

    filtered data

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import plotly_express as px
+from backend import get_data
 
 
 st.title("Weather Forecast for the Week")
@@ -13,13 +14,10 @@ option = st.selectbox("Select data to view:",
 
 st.header(f"{option} for the next {days} days in {place}")
 
-def get_data(day):
-    date = ["2024-02-02","2024-03-03", "2024-04-04"]
-    temp = [15, 23, 44]
-    temp = [day * i for i in temp]
-    return date, temp
+data  = get_data(place, days, option)
 
-d, t = get_data(days)
+
+
 
 figure = px.line(x=d, y=t, labels={"x":"Date", "y": "Temperature"})
 st.plotly_chart(figure)
