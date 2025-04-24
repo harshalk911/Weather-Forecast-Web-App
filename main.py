18da7d2228a9446ceff837f4c73bcfb523d175e1
Harshal Korade <korade54321@gmail.com>
2025-04-24 08:09:43 +0530

    added line chart using plotly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from click import style
+import plotly_express as px
+
 
 st.title("Weather Forecast for the Week")
 
@@ -12,3 +13,13 @@ option = st.selectbox("Select data to view:",
 
 st.header(f"{option} for the next {days} days in {place}")
 
+def get_data(day):
+    date = ["2024-02-02","2024-03-03", "2024-04-04"]
+    temp = [15, 23, 44]
+    temp = [day * i for i in temp]
+    return date, temp
+
+d, t = get_data(days)
+
+figure = px.line(x=d, y=t, labels={"x":"Date", "y": "Temperature"})
+st.plotly_chart(figure)
